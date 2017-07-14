7c96b7f2e33e18ef5ac5ff5e6dc26f42601346fe
Mark Delos Reyes <makizworks@gmail.com>
2017-07-14 16:35:26 -0400

    done with timer and point

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,61 @@
 console.log('You are in!')
 
-let squid = document.querySelectorAll('.squid')[1];
-
-let counter = 0
-squidPosition = 80;
+let squids = document.querySelectorAll('section')[Math.floor(Math.random() * 6)];
+squidPosition = 180;
+let score = 0;
 
 let randomSquid  = setInterval(function(){ getRandom() }, 2000);
 
 function getRandom() {
-        newRandom = Math.floor(Math.random() * 6);
-        squid = document.querySelectorAll('.squid')[newRandom];
-        console.log('random' + newRandom);
+    newRandom = Math.floor(Math.random() * 6);
+    squids = document.querySelectorAll('section')[newRandom];
+    console.log('random' + newRandom);
 }
 
-function upDown() {
-    if(squidPosition === 80) {
+function squidUpDown() {
+    if(squidPosition === 180) {
         squidPosition = 0;
-        squid.style.top = squidPosition + 'px';
+        squids.style.top = squidPosition + 'px';
     } else {
-        squidPosition = 80;
-        squid.style.top = squidPosition + 'px';
+        squidPosition = 180;
+        squids.style.top = squidPosition + 'px';
     }
-    setTimeout(upDown, 1000);
+    setTimeout(squidUpDown, 1000);
 }
 
-upDown();
+squidUpDown();
 
+function hit() {
+    console.log('Hit');
+    score++;
+    document.getElementById('score').innerHTML = score;
+}
 
+window.onload = function(){
+    document.getElementById("squid1").addEventListener("click",hit);
+    document.getElementById("squid2").addEventListener("click",hit); 
+    document.getElementById("squid3").addEventListener("click",hit); 
+    document.getElementById("squid4").addEventListener("click",hit); 
+    document.getElementById("squid5").addEventListener("click",hit); 
+    document.getElementById("squid6").addEventListener("click",hit);
+}
 
+//counter 
+function countdown() {
+    let timer = document.getElementById('timer');
+    let secs = 60;
 
+    function timerStart() {
+        if (secs === 0) {
+            clearInterval(timerStart);
+            timer.innerHTML = secs;
+        } else {
+            secs--;
+            timer.innerHTML = secs;
+        }  
+            
+    }
+    setInterval(timerStart, 1000)
+}
 
-
-
+countdown();
