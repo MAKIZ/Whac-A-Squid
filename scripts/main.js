54f1e47a777836534e2d73aea73ac9dbab5c9479
Mark Delos Reyes <makizworks@gmail.com>
2017-07-13 23:20:56 -0400

    random one at a time every refresh

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,36 @@
 console.log('You are in!')
 
 let coral = document.querySelector('.coral');
-let squid = document.querySelector('.squid');
+let squid = document.querySelectorAll('.squid');
 
-squidLocation = 80;
+let counter = 0
+squidPosition = 80;
+
+function getRandom() {
+    newRandom = Math.floor(Math.random() * squid.length);
+    squid = document.querySelectorAll('.squid')[newRandom];
+    console.log('random');
+    return squid;
+}
+
+getRandom();
 
 function upDown() {
-    // let transition = window.requestAnimationFrame(upDown);
-    
-    if(squidLocation == 80) {
-        squidLocation = 0;
-        squid.style.top = squidLocation + 'px';
+    if(squidPosition == 80) {
+        squidPosition = 0;
+        squid.style.top = squidPosition + 'px';
     } else {
-        squidLocation = 80;
-        squid.style.top = squidLocation + 'px';
+        squidPosition = 80;
+        squid.style.top = squidPosition + 'px';
     }
     setTimeout(upDown, 1000);
 }
 
 upDown();
+
+
+
+
+
+
+
